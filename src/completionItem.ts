a5161aa3a1265387e2cc4958742d36468c383a75
msbrm <cuihaniss@gmail.com>
2024-06-18 18:55:13 +0800

    fix: get python code block

--- src/completionItem.ts
+++ src/completionItem.ts
@@ -1,6 +1,36 @@
 import * as vscode from 'vscode';
 
 
+function lineValidation(
+    line: string, bracketStack: string[], strFlag: string
+): { needNextLine: boolean, bracketStack: string[], strFlag: string } {
+    let needNextLine: boolean = false;
+    for (let char of line) {
+        if (strFlag) {
+            if (char === strFlag) {
+                strFlag = '';
+            } else {
+                continue;
+            }
+        } else {
+            if ('([{'.includes(char)) {
+                bracketStack.push(char);
+            }
+            if (')]}'.includes(char)) {
+                bracketStack.pop();
+            }
+            if (char === "'" || char === '"') {
+                strFlag = char;
+            }
+        }
+    }
+    if (strFlag || bracketStack.length > 0) {
+        needNextLine = true;
+    }
+    return { needNextLine, bracketStack, strFlag };
+}
+
+
 function getLeadingWhitespace(line: string): string {
     const whitespaceRegex = /^[\s\t]*/;
     const match = line.match(whitespaceRegex);
@@ -45,11 +75,40 @@ export class PythonHandler extends FixCompletionItem {
         if (linePrefixTrim.endsWith(this.config.docTriggerCharacters.repeat(3))) {
             const previousLine = document.lineAt(position.line - 1).text;
             const leadingWhitespace = getLeadingWhitespace(previousLine);
+            const leadingWhitespaceLength = leadingWhitespace.length;
+            const tmpCodeBlock = [];
             // python class
             if (previousLine.trim().startsWith('class')) {
                 const completionItem = new vscode.CompletionItem(`abc-class: ${this.languageId}`, vscode.CompletionItemKind.Snippet);
                 const insertConfig = this.config.class;
                 let insertText = new vscode.SnippetString();
+
+                let lineIdx = position.line;
+                tmpCodeBlock.push(previousLine);
+                let needNextLine: boolean = false;
+                let bracketStack: string[] = [];
+                let strFlag: string = '';
+                while (++lineIdx <= document.lineCount) {
+                    const tmpLine = document.lineAt(lineIdx).text;
+                    if (tmpLine.trim().length === 0) {
+                        continue;
+                    }
+
+                    if (getLeadingWhitespace(tmpLine).length >= leadingWhitespaceLength || needNextLine) {
+                        tmpCodeBlock.push(tmpLine);
+                        const {
+                            needNextLine: tmpNeedNextLine,
+                            bracketStack: tmpBracketStack,
+                            strFlag: tmpStrFlag
+                        } = lineValidation(tmpLine.trim(), bracketStack, strFlag);
+                        needNextLine = tmpNeedNextLine;
+                        bracketStack = tmpBracketStack;
+                        strFlag = tmpStrFlag;
+                    } else {
+                        break;
+                    }
+                }
+                console.log(tmpCodeBlock);
                 for (const item of insertConfig) {
                     // insertText.appendText(leadingWhitespace);
                     insertText.appendText(item);
